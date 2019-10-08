74b20867a5f28a3b1fdf02d4943c241864e57591
KaosEngineer <Deep.exe5>
2019-10-08 23:55:11 +0300

    Added choice of optimizer.

--- prior_networks/priornet/run/train_dpn.py
+++ prior_networks/priornet/run/train_dpn.py
@@ -144,7 +144,7 @@ def main():
     if len(train_dataset) < len(ood_dataset):
         ratio = np.round(float(len(ood_dataset)) / float(len(train_dataset)))
         assert ratio.is_integer()
-        dataset_list = [train_dataset, ] * (1 + int(ratio))
+        dataset_list = [train_dataset, ] * int(ratio)
         train_dataset = data.ConcatDataset(dataset_list)
     elif len(train_dataset) > len(ood_dataset):
         ratio = float(len(train_dataset)) / float(len(ood_dataset))
